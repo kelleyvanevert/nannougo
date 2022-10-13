c7dd69accd29ce1ca00c3f1d1cd4012a480629fa
Kelley van Evert <hello@klve.nl>
2022-10-13 14:47:05 +0200

    reorganized a bit

--- src/main.rs
+++ src/main.rs
@@ -13,52 +13,65 @@ fn main() {
         .run();
 }
 
-struct Model {
-    game: Game,
+struct Measurements {
+    stone_size: f32,
 }
 
-fn model(app: &App) -> Model {
-    app.set_loop_mode(LoopMode::Wait);
+struct ViewModel {
+    game: Game,
+    rect: Rect,
+}
 
-    Model {
-        game: Game::new(13),
+impl ViewModel {
+    fn new(size: usize, rect: Rect) -> Self {
+        Self {
+            game: Game::new(size),
+            rect,
+        }
+    }
+
+    fn calculate_measurements(&self) -> Measurements {
+        let board_rect = self.rect.pad(24.0);
+        let (w, h) = board_rect.w_h();
+        let board_size = if w < h { w } else { h };
+        let stone_size = board_size / (self.game.size as f32);
+
+        Measurements { stone_size }
+    }
+
+    fn stone_project(&self, p: Pos) -> Vec2 {
+        let m = self.calculate_measurements();
+
+        let d = (self.game.size as f32 - 1.0) / 2.0;
+        vec2(
+            (p.0 as f32 - d) * m.stone_size,
+            (p.1 as f32 - d) * m.stone_size,
+        )
+    }
+
+    fn stone_unproject(&self, v: Vec2) -> Option<Pos> {
+        let m = self.calculate_measurements();
+
+        let d = (self.game.size as f32 - 1.0) / 2.0;
+        let x: i32 = (v.x / m.stone_size + d).round() as i32;
+        let y: i32 = (v.y / m.stone_size + d).round() as i32;
+        if x >= 0 && x < (self.game.size as i32) && y >= 0 && y < (self.game.size as i32) {
+            Some(Pos(x, y))
+        } else {
+            None
+        }
     }
 }
 
-fn scale(
-    size: usize,
-    rect: Rect<f32>,
-) -> (
-    Box<dyn Fn(Pos) -> Vec2>,
-    Box<dyn Fn(f32, f32) -> Option<Pos>>,
-) {
-    let board = rect.pad(16.0);
-    let (w, h) = board.w_h();
-    let board_size = if w < h { w } else { h };
-    let stone_size = board_size / (size as f32);
+fn model(app: &App) -> ViewModel {
+    app.set_loop_mode(LoopMode::Wait);
 
-    let half = ((size as f32) - 1.0) / 2.0;
-
-    (
-        Box::new(move |p: Pos| {
-            vec2(
-                ((p.0 as f32) - half) * stone_size,
-                ((p.1 as f32) - half) * stone_size,
-            )
-        }),
-        Box::new(move |fx: f32, fy: f32| {
-            let x: i32 = (fx / stone_size + half).round() as i32;
-            let y: i32 = (fy / stone_size + half).round() as i32;
-            if x >= 0 && x < (size as i32) && y >= 0 && y < (size as i32) {
-                Some(Pos(x, y))
-            } else {
-                None
-            }
-        }),
-    )
+    ViewModel::new(13, app.window_rect())
 }
 
-fn event(app: &App, model: &mut Model, event: Event) {
+fn event(app: &App, model: &mut ViewModel, event: Event) {
+    model.rect = app.window_rect();
+
     match event {
         Event::DeviceEvent(
             _,
@@ -67,8 +80,7 @@ fn event(app: &App, model: &mut Model, event: Event) {
                 ..
             },
         ) => {
-            let (_place, unplace) = scale(model.game.size, app.window_rect());
-            let mouseat = unplace(app.mouse.x, app.mouse.y);
+            let mouseat = model.stone_unproject(vec2(app.mouse.x, app.mouse.y));
             if let Some(p) = mouseat {
                 model.game.try_place_stone(p);
             }
@@ -77,25 +89,22 @@ fn event(app: &App, model: &mut Model, event: Event) {
     }
 }
 
-fn view(app: &App, model: &Model, frame: Frame) {
-    let rect = app.window_rect().pad(16.0);
-    let (w, h) = rect.w_h();
-    let board_size = if w < h { w } else { h };
-    let stone_size = board_size / (model.game.size as f32);
-
-    let (place, unplace) = scale(model.game.size, app.window_rect());
+fn view(app: &App, model: &ViewModel, frame: Frame) {
+    let m = model.calculate_measurements();
 
     let draw = app.draw();
     draw.background().color(WHITE);
 
-    let mouseat = unplace(app.mouse.x, app.mouse.y);
+    let mouseat = model.stone_unproject(vec2(app.mouse.x, app.mouse.y));
 
     for x in 0..model.game.size {
         for y in 0..model.game.size {
+            let pos = Pos(x as i32, y as i32);
+
             let stone = draw
                 .ellipse()
-                .xy(place(Pos(x as i32, y as i32)))
-                .w_h(stone_size - 4.0, stone_size - 4.0);
+                .xy(model.stone_project(pos))
+                .w_h(m.stone_size - 4.0, m.stone_size - 4.0);
 
             match model.game.board[y * model.game.size + x] {
                 None => {
@@ -117,8 +126,8 @@ fn view(app: &App, model: &Model, frame: Frame) {
     if let Some(p) = mouseat {
         if !model.game.has_stone_at(p) {
             draw.ellipse()
-                .xy(place(p))
-                .w_h(stone_size + 8.0, stone_size + 8.0)
+                .xy(model.stone_project(p))
+                .w_h(m.stone_size + 8.0, m.stone_size + 8.0)
                 .color(if model.game.turn { BLACK } else { WHITE })
                 .stroke(BLACK)
                 .stroke_weight(3.0);
